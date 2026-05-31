be9b99039ca22006290c7f2fee44aa73bfa14ad7
Suman <suman.thangadurai@edu.dsti.institute>
2026-05-31 13:50:32 +0200

    fixed threshold comments and cleaned up

--- vader_analysis.py
+++ vader_analysis.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# tried textblob first but the scores were all over the place
+# tried textblob first but it was not good
 # from textblob import TextBlob
+
 # cleaned up a bit 
 analyzer = SentimentIntensityAnalyzer()
 
@@ -10,7 +11,7 @@ def analyze_vader(text):
     scores = analyzer.polarity_scores(text)
     compound = scores['compound']
 
-    # 0.05 threshold from vader docs, seemed to work ok on test data
+    # 0.05 is from vader docs, seemed to work ok on test data
     if compound >= 0.05:
         return 'positive', compound
     elif compound <= -0.05:
@@ -19,11 +20,11 @@ def analyze_vader(text):
         return 'neutral', compound
 
 def run_vader(df):
-    # TODO: maybe add progress bar here for large datasets
+    # TODO: maybe add progress bar for big data
     df['vader_sentiment'] = df['clean_tweet'].apply(analyze_vader)
     print(df[['clean_tweet', 'vader_sentiment']].head(15))
     return df
 
-# quick test
+#  for quick test
 # df = pd.read_csv('new_train_data_s140.csv')
 # run_vader(df)
