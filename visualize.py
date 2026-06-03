58cc196573c9ad02faa260565ee187937475f006
Suman <suman.thangadurai@edu.dsti.institute>
2026-06-03 14:28:59 +0200

    visualized the results using pie chart

--- visualize.py
+++ visualize.py
@@ -1,32 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_sentiment_distribution(df,model_name):
-
-    #To count sentiment
+def plot_sentiment_distribution(df, model_name):
     if 'vader_sentiment' in df.columns:
-        counts=df['vader_sentiment'].value_counts()
+        counts = df['vader_sentiment'].value_counts()
     else:
-        counts=df['roberta_sentiment'].value_counts()
+        counts = df['roberta_sentiment'].value_counts()
 
-    #To create pie chart
+    # tried bar chart first but pie chart looked better for this
     plt.figure(figsize=(8,6))
     plt.pie(counts,
             labels=counts.index,
             autopct='%1.1f%%',
             colors=['green','red','blue'])
+
     plt.title(f'{model_name} Sentiment Distribution')
     plt.savefig(f'{model_name}_distribution.png')
     plt.show()
-    print(f"Saver{model_name}_distribution.png!")
+    print(f"saved {model_name}_distribution.png")
 
-#To load and visualize
-df_vader=pd.read_csv('vader_results.csv')
-df_roberta=pd.read_csv('roberta_result.csv')
+# load the results and make charts
+df_vader = pd.read_csv('vader_results.csv')
+df_roberta = pd.read_csv('roberta_result.csv')
 
-#plot VADER distribution
+# TODO: colors dont match between both charts ,needs to fixed 
+
 plot_sentiment_distribution(df_vader, 'VADER')
-
-#plot RoBERTa distribution
 plot_sentiment_distribution(df_roberta, 'RoBERTa')
-
